b90d7933b15a6e83dd93cb8f6e666c62a3ceae01
Matthew B. Jané <52077481+MatthewBJane@users.noreply.github.com>
2025-09-23 13:14:54 -0400

    update

---
+++ R/my_function.R
@@ -0,0 +1,15 @@
+#' My Function Title
+#'
+#' A more detailed description of what the function does.
+#'
+#' @param x A numeric vector.
+#' @param y A numeric vector.
+#' @return A numeric vector representing the sum of x and y.
+#' @examples
+#' my_function(1:3, 4:6)
+my_function <- function(x, y) {
+  x + y
+}
+
+
+# When done with editing the function and documentation simply run "roxygen2::roxygenise()" to generate the markdown documentation in the man folder
