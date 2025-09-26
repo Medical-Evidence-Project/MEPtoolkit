9886b1274ce72cd8b77ed06272c96e073258439e
Nathanael Larigaldie <larigaldie.n@gmail.com>
2025-09-26 21:04:11 +0200

    Refactored statistic validation for rounding into general functions
    Added tests for stat validation
    
    Changed t_slicer and related tests accordingly

---
+++ R/rounding_validation.R
@@ -0,0 +1,108 @@
+#' decimal_places
+#'
+#' Finds and returns the number of decimal places on a statistic
+#'
+#' @param x Character string. The statistic to examine
+#' @return The number of decimal places
+#' @examples
+#' decimal_places("3")
+#' decimal_places(".984")
+#' decimal_places("2.00")
+decimal_places <- function(x) {
+  if (grepl("\\.", x))
+    return(nchar(strsplit(x, "\\.")[[1]][2]))
+  else
+    return(0)
+}
+
+#' validation_descriptive
+#'
+#' Validates a descriptive statistic: checks it is a string that can be
+#' converted to a number, and returns the possible range of values if it was
+#' rounded
+#'
+#' @param statistic Character string. The statistic to validate
+#' @param name Character string. The name of the variable (for error output)
+#' @return A list of numbers: the possible range of values (minimum, original
+#' and maximum)
+#' @examples
+#' validation_descriptive("2.14", "m1")
+validation_descriptive <- function (statistic, name)
+{
+  if(!is.character(statistic)) {
+    stop(paste(name, "must be a character string."))
+  }
+  else {
+    suppressWarnings({
+      statistic_num <- as.numeric(statistic)
+      if(is.na(statistic_num)) {
+        stop(paste(name, "must be convertible to a number."))
+      }
+    }
+    )
+  }
+  statistic_range <- list(
+    minimum = statistic_num - 5*10^(-decimal_places(statistic)-1),
+    original = statistic_num,
+    maximum = statistic_num + 5*10^(-decimal_places(statistic)-1)
+  )
+  return(statistic_range)
+}
+
+#' validation_descriptive
+#'
+#' Validates a p statistic: checks it is a string that can be
+#' converted to a number, and returns the possible range of values if it was
+#' rounded
+#'
+#' @param p Character string. The p value to validate
+#' @param name Character string. Optional. The name of the variable
+#' (for error output)
+#' @return A list of numbers: the possible range of values (minimum, original
+#' and maximum)
+#' @examples
+#' validation_p(".14")
+#' p_value <- ".25"
+#' validation_p(p_value, "p_value")
+#' validation_p("<.001")
+validation_p <- function(p, name = "p")
+{
+  if(!is.character(p)) {
+    stop(paste(name, "must be a character string."))
+  }
+  else {
+    suppressWarnings({
+      # TODO: Deal with "<.X"
+      if((grepl("<", p))) {
+        p <- strsplit(p, "<")[[1]][2]
+        p_inferior <- TRUE
+      }
+      else
+        p_inferior <- FALSE
+      p_num <- as.numeric(p)
+      if(is.na(as.numeric(p_num))) {
+        stop(paste(name, "must be convertible to a number."))
+      }
+    }
+    )
+  }
+  if(p_num > 1 || p_num < 0)
+  {
+    stop(paste(name, "must be between 0 and 1."))
+  }
+  if(p_inferior == FALSE) {
+    p_range <- list(
+      minimum = max(0, p_num - 5*10^(-decimal_places(p)-1)),
+      original = p_num,
+      maximum = min(1, p_num + 5*10^(-decimal_places(p)-1))
+    )
+  }
+  else {
+    p_range <- list(
+      minimum = .Machine$double.xmin,
+      original = p_num,
+      maximum = p_num
+    )
+  }
+  return(p_range)
+}
--- tests/testthat/test-t_slicer.R
+++ tests/testthat/test-t_slicer.R
@@ -1,10 +1,3 @@
-test_that("Stops when means/SDs/p are not numbers given as strings", {
-  expect_error(t_slicer(1.20, "1.2", 60, "2.1", "2.5", 60, ".032"),
-               "m1, s1, m2 and s2 must be character vectors.")
-  expect_error(t_slicer("1.20c", "1.2", 60, "2.1", "2.5", 60, ".032"),
-               "m1, s1, m2, s2 and p must be interpretable as numbers.")
-})
-
 test_that("The 4 combinations of output are possible", {
   expect_identical(t_slicer("1.20", "1.2", 60, "2.1", "2.5", 30, ".02"),
                    list(Student=TRUE, Welch=FALSE))
